bc3f072d4a01fbc774d33656afd0cf77c5152694
mikeyim337 <mk337337@gmail.com>
2020-10-10 23:16:32 -0700

    done with mvc module

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get("/add-product", productsController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product.pug", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+// /admin/add-product => POST
+router.post("/add-product", productsController.postAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
