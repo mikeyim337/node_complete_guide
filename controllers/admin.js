428653a148aecf964a641ad6f45a8652aca806ff
mikeyim337 <mk337337@gmail.com>
2020-10-22 01:16:15 -0700

    done with adding authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -64,21 +64,24 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
-      return product.save(); //this will update the product
+      return product.save().then((result) => {
+        console.log("updated product"); //second promise for calling the db and updating
+        res.redirect("/admin/products");
+      }); //this will update the product
     })
-    .then((result) => {
-      console.log("updated product"); //second promise for calling the db and updating
-      res.redirect("/admin/products");
-    })
+
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id }) //authoriazation filtering
     //.select('title price -_id') //gets title, price MINUS id
     //.populate("userId", "title")         //populates only the userId and title //without the second parameter populates the whole object with userId (querying using userId)
     .then((products) => {
@@ -93,7 +96,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then((result) => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
